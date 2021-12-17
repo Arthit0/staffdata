789a996da9582dd39eae011cd3ae749990cc9046
Arthit0 <88313738+Arthit0@users.noreply.github.com>
2021-12-17 10:06:20 +0700

    connection to data base
    connection to data base

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php
+$serverName = "10.8.98.11";
+$connectionInfo = array( "Database"=>"MOC_DriveDashboardDB", "UID"=>"DITPStuff", "PWD"=>"DITPP@ssw0rd!@#$" , "CharacterSet" => "UTF-8");
+$conn_ditp = sqlsrv_connect( $serverName, $connectionInfo);
+if(!$conn) {
+    die( print_r( sqlsrv_errors(), true));
+}else{
+
+    echo "connected";
+}
+
+
+
+
+$mysqli = new mysqli("10.8.99.131","oneuat","Ibusiness19#","oneuat");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
+
