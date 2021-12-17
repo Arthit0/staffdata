a0fe6e78c272ed80fe1f658fc544f76306142fd3
Arthit0 <88313738+Arthit0@users.noreply.github.com>
2021-12-17 10:17:48 +0700

    test select data

--- DITPStuff_DimActivities.php
+++ DITPStuff_DimActivities.php
@@ -1,6 +1,15 @@
 <?php 
 include('connection.php');
-echo"Hi";
+$sql12="select * from DITPStuff_DimActivities";
+$query_cr2 = sqlsrv_query($conn, $sql12, array(), array("Scrollable" => 'static'));  
+if (sqlsrv_num_rows($query_cr2) >0){
+    while ($data_loop = sqlsrv_fetch_array($query_cr2, SQLSRV_FETCH_ASSOC)){
+        print_r($data_loop);
+        die();
+        
+    }
+
+}
 
 
 ?>
