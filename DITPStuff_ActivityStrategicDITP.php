57e7fc261188738115bc66f3f0d9ebecb00e569d
Arthit0 <88313738+Arthit0@users.noreply.github.com>
2021-12-20 09:29:11 +0700

    Save data in Server
    table DITPStuff_ActivityStrategicDITP

---
+++ DITPStuff_ActivityStrategicDITP.php
@@ -0,0 +1,24 @@
+<?php
+include("connection.php");
+ini_set("max_execution_time",0);
+$TRUNCATE = "TRUNCATE ditpstuff_activitybudget";
+    if ($mysqli->query($TRUNCATE) === FALSE ) {
+    echo "Failed to connect to MySQL: " . $mysqli->error;
+    }
+$SqlServer = "select * from DITPStuff_ActivityStrategicDITP";
+$query_cr2 = sqlsrv_query($conn_ditp, $SqlServer, array(), array("Scrollable" => 'static'));  
+if (sqlsrv_num_rows($query_cr2) >0){
+    while ($data_loop = sqlsrv_fetch_array($query_cr2, SQLSRV_FETCH_ASSOC)){
+        
+        $DITP_ONE = "INSERT INTO `ditpstuff_activitystrategicditp`(`StrategicDITPRowNo`, `Activity_id`, `Strategic_id`, `Strategic_Name`) VALUES ('".$data_loop['StrategicDITPRowNo']."','".$data_loop['Activity_id']."','".$data_loop['Strategic_id']."','".$data_loop['Strategic_Name']."')";
+        print_r($DITP_ONE);
+        if ($mysqli->query($DITP_ONE) === FALSE ) {
+            echo "Failed to connect to MySQL: " . $mysqli->error;
+        // print_r($data_loop);
+            die();
+        } 
+    }
+}
+echo "Success!!"
+
+?>
