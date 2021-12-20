9f3d59840256bbf07dd5c147a2a1262e61fb5551
Arthit0 <88313738+Arthit0@users.noreply.github.com>
2021-12-20 10:33:57 +0700

    Update

--- DITP_ActivityStrategicMOC.php
+++ DITP_ActivityStrategicMOC.php
@@ -1,23 +1,21 @@
 <?php
 include("connection.php");
 ini_set("max_execution_time");
-// $TRUNCATE = "TRUNCATE DITPStuff_ActivityStrategicMOC";
-//     if ($mysqli->query($TRUNCATE) === FALSE ) {
-//     echo "Failed to connect to MySQL: " . $mysqli->error;
-//     }
+$TRUNCATE = "TRUNCATE DITPStuff_ActivityStrategicMOC";
+    if ($mysqli->query($TRUNCATE) === FALSE ) {
+    echo "Failed to connect to MySQL: " . $mysqli->error;
+    }
 $SqlServer = "select * from DITPStuff_ActivityStrategicMOC";
 $query_cr2 = sqlsrv_query($conn_ditp, $SqlServer, array(), array("Scrollable" => 'static'));  
 if (sqlsrv_num_rows($query_cr2) >0){
     while ($data_loop = sqlsrv_fetch_array($query_cr2, SQLSRV_FETCH_ASSOC)){
-        
-        // $DITP_ONE = "INSERT INTO `ditpstuff_activitystrategicditp`(`StrategicDITPRowNo`, `Activity_id`, `Strategic_id`, `Strategic_Name`) VALUES ('".$data_loop['StrategicDITPRowNo']."','".$data_loop['Activity_id']."','".$data_loop['Strategic_id']."','".$data_loop['Strategic_Name']."')";
-        // print_r($DITP_ONE);
-        // if ($mysqli->query($DITP_ONE) === FALSE ) {
-        //     echo "Failed to connect to MySQL: " . $mysqli->error;
-        print_r($data_loop);
+        $DITP_ONE = "INSERT INTO `ditpstuff_activitystrategicmoc`(`StrategicMOCRowNo`, `Activity_id`, `Strategic_id`, `Strategic_Name`) VALUES ('".$data_loop['StrategicMOCRowNo']."','".$data_loop['Activity_id']."','".$data_loop['Strategic_id']."','".$data_loop['Strategic_Name']."')";
+        print_r($DITP_ONE);
+        if ($mysqli->query($DITP_ONE) === FALSE ) {
+            echo "Failed to connect to MySQL: " . $mysqli->error;
             die();
-        // } 
+        } 
     }
 }
-// echo "Success!!"
+echo "Success!!"
 ?>
