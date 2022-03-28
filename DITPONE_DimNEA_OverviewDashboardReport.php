73c9e3d0e4a97d957ef4008847f3a8fbd220170f
Arthit0 <88313738+Arthit0@users.noreply.github.com>
2022-03-28 09:29:56 +0700

    test

--- DITPONE_DimNEA_OverviewDashboardReport.php
+++ DITPONE_DimNEA_OverviewDashboardReport.php
@@ -2,6 +2,10 @@
 include("connection_NEA.php");
 include("helper.php");
 ini_set("max_execution_time",0);
+$TRUNCATE = "TRUNCATE ditpone_dimnea_overviewdashboardreport";
+    if ($mysqli->query($TRUNCATE) === FALSE ) {
+    echo "Failed to connect to MySQL: " . $mysqli->error;
+    } 
 $SqlServer = "select * from DITPONE_DimNEA_OverviewDashboardReport";
 $query_cr2 = sqlsrv_query($conn_ditp, $SqlServer, array(), array("Scrollable" => 'static'));  
 if (sqlsrv_num_rows($query_cr2) >0){
